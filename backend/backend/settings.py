9501fcc6aae93ab9bcd2ccb3582af07618bbd7e7
AREEBA-FATIMA <areebafatima2457@gmail.com>
2025-09-30 17:13:24 +0500

    centralized database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,9 +109,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myprojectdb',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'sis',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
