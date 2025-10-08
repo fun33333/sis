01058d9cf20d60d9487cb68200dd482b12cda208
emp10 <emp10@ait.com>
2025-10-08 17:06:37 +0500

    worked on class teachers mainly

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,25 +81,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',  # ye add karo
-#         'NAME': os.getenv('DB_NAME', 'project_db'),
-#         'USER': os.getenv('DB_USER', 'project_user'),
-#         'PASSWORD': os.getenv('DB_PASSWORD', 'project_pass'),
-#         'HOST': os.getenv('DB_HOST', 'localhost'),
-#         'PORT': os.getenv('DB_PORT', '5432'),
-#         'CONN_MAX_AGE': 0,  # Disable connection pooling
-#         'OPTIONS': {
-#             'connect_timeout': 10,
-#         },
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # ye add karo
+        'NAME': 'sis-db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 0,  # Disable connection pooling
+        'OPTIONS': {
+            'connect_timeout': 10,
+        },
     }
 }
 
